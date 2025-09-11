09304613f936ca6d385b0ac84e1b3b250ebea859
Artur <arturpztlopes@gmail.com>
2025-09-11 09:32:04 -0300

    layout initial

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
-use App\Models\User;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', function () {
-    $admin = User::with('detail', 'department')->find(1);
-
-   return view('admin', compact('admin'));
-});
+Route::view('login', 'auth.login');
